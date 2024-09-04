32eb5d5a60c8c49ee009feeafd89473a9cfa70c6
arnonr <arnonr@kmutnb.ac.th>
2024-09-04 16:05:51 +0700

    up

--- plugins/google-analytics.client.js
+++ plugins/google-analytics.client.js
@@ -1,16 +1,15 @@
 export default defineNuxtPlugin((nuxtApp) => {
-    if (process.client) {
-      (function() {
-        const script = document.createElement('script');
+    (function () {
+        const script = document.createElement("script");
         script.async = true;
         script.src = `https://www.googletagmanager.com/gtag/js?id=G-6TTDPNWB3D`;
         document.head.appendChild(script);
-  
+
         window.dataLayer = window.dataLayer || [];
-        function gtag() { dataLayer.push(arguments); }
-        gtag('js', new Date());
-        gtag('config', 'G-6TTDPNWB3D');
-      })();
-    }
-  });
-  
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag("js", new Date());
+        gtag("config", "G-6TTDPNWB3D");
+    })();
+});
