a64094a34ef809819caaceda71d801df98cffce3
arnonr <arnonr@kmutnb.ac.th>
2024-11-19 16:27:49 +0700

    update

--- components/home/Search.vue
+++ components/home/Search.vue
@@ -90,10 +90,21 @@
                         >
                             <ListGridItem2
                                 :item="{
-                                    link: 'serve/',
+                                  link: 'serve/',
                                     id: it.id,
                                     title: it.title,
                                     file: it.serve_file,
+                                    type: it.type,
+                                    department: it.department,
+                                    price1: it.price1,
+                                    price2: it.price2,
+                                    price3: it.price3,
+                                    price4: it.price4,
+                                    price5: it.price5,
+                                    unit_th: it.unit_th,
+                                    phone: it.phone,
+                                    email: it.email,
+                                    breif_detail_th: it.breif_detail_th,
                                 }"
                             />
                         </div>
