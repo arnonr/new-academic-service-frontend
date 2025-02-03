e1df247c45ae13821cbf5cc5bbc12a6018f41beb
arnonr <arnonr@kmutnb.ac.th>
2025-02-03 16:45:25 +0700

    useCookie("academic_service_token")

--- layouts/headers/HeaderHomeCustom.vue
+++ layouts/headers/HeaderHomeCustom.vue
@@ -246,7 +246,7 @@ export default {
     data() {
         return {
             isSticky: false,
-            authenticated: useCookie("academic_service_token");
+            authenticated: useCookie("academic_service_token"),
             isModalVisible: false,
         };
     },
