e61cf6f0d31095dcc50dcd34960682cde257b004
arnonr <arnonr@kmutnb.ac.th>
2025-02-03 16:54:34 +0700

    up

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -2,10 +2,10 @@ import { useRouter } from "vue-router";
 
 export default defineNuxtRouteMiddleware((to, from) => {
     if (process.server) return; // กัน error บน server-side
-
     const token = useCookie("academic_service_token");
+    console.log(token.value)
 
-    if (!token) {
+    if (!token.value) {
         window.location.href = "/";
     }
 });
